8c19b219fa8fb1a14b882621c365eaeaeaf883ed
uhiaha888 <uhiaha888@gmail.com>
2025-05-25 04:42:18 +0900

    list all

--- internal/commands/list.go
+++ internal/commands/list.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/TakahashiShuuhei/todo/internal/models"
@@ -44,10 +45,10 @@ func PrintTodos(todos *models.TodoList, showArchived bool) {
 }
 
 var listCmd = &cobra.Command{
-	Use:     "list",
+	Use:     "list [all]",
 	Aliases: []string{"l"},
 	Short:   "List all tasks",
-	Long:    `List all tasks in the todo list.`,
+	Long:    `List all tasks in the todo list. Use 'list all' to show archived tasks as well.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Todoファイルを探す
 		todoPath, err := utils.FindTodoFile()
@@ -63,6 +64,9 @@ var listCmd = &cobra.Command{
 		if err != nil {
 			return fmt.Errorf("failed to load todos: %w", err)
 		}
+
+		// "all" が指定されている場合はアーカイブされたタスクも表示
+		showArchived = len(args) > 0 && strings.ToLower(args[0]) == "all"
 
 		PrintTodos(todos, showArchived)
 		return nil
